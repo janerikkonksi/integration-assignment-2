e309aba526a9f78a99ac80d21371ab08d95ec751
Kerdo Kurs <kerdokurs@gmail.com>
2024-03-17 17:46:34 +0200

    add OpenAPI docs

--- customer-service/src/main/java/ee/ut/esi/customers/CustomersApplication.java
+++ customer-service/src/main/java/ee/ut/esi/customers/CustomersApplication.java
@@ -1,5 +1,9 @@
 package ee.ut.esi.customers;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -25,4 +29,15 @@ public class CustomersApplication {
         return new ModelMapper();
     }
 
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+            .info(new Info().title("Customers - Warehouse API")
+                .description("Customers - Warehouse API")
+                .version("v0.0.1")
+                .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+            .externalDocs(new ExternalDocumentation()
+                .description("Warehouse API - Customers Documentation")
+                .url("http://localhost:8082/swagger-ui/index.html"));
+    }
 }
